a5cb0c653e6a493161f7fa0f991ead8cff6b5762
Sachin1Thapa <yousachin328gmail.com>
2024-02-04 00:21:26 +0545

    book

--- logsin/Book/views.py
+++ logsin/Book/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+
 @login_required(login_url='login')
 def library(request):
     return render(request , 'library.html')
--- logsin/registration/views.py
+++ logsin/registration/views.py
@@ -36,33 +36,15 @@ def signupdata(request):
         if pass1 != pass2:
             error_message = 'Password doesnot match '
         else:
-            user = Userdata.objects.create(
-                name=name, email=email, password=hashed_password)
-            return redirect('login')
+            if Userdata.objects.filter(name = name ).exists():
+                error_message = "Name already Used  "
+            elif Userdata.objects.filter(email = email ).exists():
+                error_message = "Email already taken"
+            else : 
+                user = Userdata.objects.create(name=name, email=email, password=hashed_password)
+                return redirect('login')
 
     return render(request, 'signup.html', {'errmsg': error_message})
-
-
-def logindata(request):
-
-    error_message = None
-
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['pass']
-
-        user = authenticate(request, email=email, password=password)
-
-        print("User:", user)
-
-        if user is None:
-            error_message = "Email or the Password is incorrect Re enter"
-
-        else:
-            login(request, user)
-            return redirect('msg')
-
-    return render(request, 'login.html', {'errmsg': error_message})
 
 
 def logindata(request):
